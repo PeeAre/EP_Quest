cfa7f7b3db10b3057d7f62078216198a89e90ab1
PeeAre <silencetray@gmail.com>
2023-12-22 16:50:09 +0100

    postcard scene changed

--- EP_Quest/wwwroot/js/Render.js
+++ EP_Quest/wwwroot/js/Render.js
@@ -6,8 +6,6 @@ var engine;
 var scene;
 var camera;
 var glitchPostProcess;
-var hemisphericLight;
-var pointLight;
 
 async function RenderOnCanvasAsync(canvasId, scenePath) {
     InitScene(canvasId);
@@ -26,23 +24,14 @@ function InitScene(canvasId) {
     canvas = document.getElementById(canvasId);
     engine = new BABYLON.Engine(canvas, true);
     scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color4(0.1, 0.1, 0.1, 1);
-    hemisphericLight = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-    hemisphericLight.intensity = 0.8;
-    pointLight = new BABYLON.PointLight("pointLight", new BABYLON.Vector3(-12, 1, 1), scene);
-    pointLight.intensity = 0; // Интенсивность света (от 0 до 1)
-    pointLight.diffuse = new BABYLON.Color3(1, 1, 1); // Цвет рассеянного света
-    pointLight.specular = new BABYLON.Color3(1, 1, 1); // Цвет отраженного света
-    pointLight.range = 120; // Максимальная дистанция, на которой свет будет воздействовать
-
 }
 async function LoadScene(scenePath) {
     await BABYLON.SceneLoader.ImportMeshAsync("", "", scenePath, scene)
         .then(() => {
             camera = scene.activeCamera = scene.cameras[0];
             camera.minZ = 0.01;
-            scene.animationGroups.forEach(x => x.stop());
-            scene.animationGroups.forEach(x => x.start());
+            //scene.animationGroups.forEach(x => x.stop());
+            //scene.animationGroups.forEach(x => x.start());
         });
     await DotNet.invokeMethodAsync('EP_Quest', 'OnSceneReady');
 }
@@ -55,17 +44,16 @@ function AddPostProcess() {
 }
 
 function SwitchGlitch() {
-    if (camera._postProcesses.length > 0 && camera._postProcesses.find(x => x.name === "Glitch")) {
+    if (camera._postProcesses.find(x => x.name === "Glitch")) {
         camera._postProcesses.length = camera._postProcesses.length === 1 ? 0 : camera._postProcesses.length;
         camera.detachPostProcess(glitchPostProcess);
-        hemisphericLight.intensity = 0.8;
-        pointLight.intensity = 0;
-        scene.clearColor = new BABYLON.Color4(0.5, 0.5, 0.5, 1);
+        scene.lights.forEach(x => x.intensity = 0.4);
         return;
     }
 
+    //scene.lights.forEach(x => x.intensity = 1.0);
+    //scene.lights.find(x => x.name === "Point").range = 3.0;
+    //scene.lights.find(x => x.name === "Point.001").range = 4.0;
+    //scene.lights.find(x => x.name === 'Sun').intensity = 0;
     camera.attachPostProcess(glitchPostProcess);
-    hemisphericLight.intensity = 0.025;
-    pointLight.intensity = 1.0;
-    scene.clearColor = new BABYLON.Color4(0.1, 0.1, 0.1, 1);
 }
