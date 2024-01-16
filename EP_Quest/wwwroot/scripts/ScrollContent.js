1044956b2493a4cd372d8bcd21a2aa427bff23b6
PeeAre <silencetray@gmail.com>
2024-01-16 21:36:16 +0100

    Added certificate

---
+++ EP_Quest/wwwroot/scripts/ScrollContent.js
@@ -0,0 +1,8 @@
+﻿function scrollToElement(containerId, elementId) {
+    let container = document.getElementById(containerId);
+    let element = document.getElementById(elementId);
+
+    if (container && element) {
+        container.scrollTo({ behavior: "smooth", top: element.offsetTop });
+    }
+}
