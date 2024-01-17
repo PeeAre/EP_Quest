a2eae355d3dc37df8f47052a5c9b340ca4da90b9
PeeAre <silencetray@gmail.com>
2024-01-17 20:37:41 +0100

    Added authorize without cookies

--- EP_Quest/Controllers/HomeController.cs
+++ EP_Quest/Controllers/HomeController.cs
@@ -4,12 +4,25 @@ namespace EP_Quest.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [HttpGet]
         public IActionResult Index()
         {
             ViewBag.Title = "Your Gift";
 
+            //if (!Request.Cookies.ContainsKey("KeyPhrase"))
+            //    Response.Cookies.Append("KeyPhrase", _configuration["Phrases:Key phrase"]);
+
             return View();
+        }
+        public void Login()
+        {
+            Response.Cookies.Append("KeyPhrase", _configuration["Phrases:Key phrase"]);
         }
     }
 }
--- EP_Quest/Startup.cs
+++ EP_Quest/Startup.cs
@@ -19,6 +19,8 @@ namespace EP_Quest
             services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddServerSideBlazor();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddHttpContextAccessor();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddDbContext<QuestDbContext>(options =>
             {
                 options.UseNpgsql(Configuration["ConnectionStrings:PostgreSQL_Connection"]);
