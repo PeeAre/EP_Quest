4b6aaa1648185d1a67a79c241f686dbe4c7efc70
PeeAre <silencetray@gmail.com>
2024-01-18 09:59:13 +0100

    Added image for page

---
+++ EP_Quest/wwwroot/scripts/Flipbook.js
@@ -0,0 +1,29 @@
+﻿function bookInit() {
+    $(document).ready(() => {
+        $("#flipbook").turn({
+            duration: 1800,
+            autoCenter: true
+        });
+        disableTurn();
+    });
+}
+
+function enableTurn() {
+    $("#flipbook").turn("disable", false);
+}
+
+function disableTurn() {
+    $("#flipbook").turn("disable", true);
+}
+
+function nextPage() {
+    $("#flipbook").turn("next");
+}
+
+function previousPage() {
+    $("#flipbook").turn("previous");
+}
+
+function zoomBook() {
+    $("#flipbook").turn("zoom", 2);
+}
