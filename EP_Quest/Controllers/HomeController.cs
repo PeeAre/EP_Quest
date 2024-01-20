c5ec4504735ab2cd9e3c0692ca2e8ef70f1cf2d8
PeeAre <silencetray@gmail.com>
2024-01-20 05:15:39 +0100

    Added notification after logging

--- EP_Quest/Controllers/HomeController.cs
+++ EP_Quest/Controllers/HomeController.cs
@@ -6,19 +6,15 @@ namespace EP_Quest.Controllers
     public class HomeController : Controller
     {
         private readonly IConfiguration _configuration;
-        private TelegramSender _telegramSender;
 
         public HomeController(IConfiguration configuration, TelegramSender telegramSender)
         {
             _configuration = configuration;
-            _telegramSender = telegramSender;
         }
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             ViewBag.Title = "Your Journey";
-
-            await _telegramSender.SendAsync("Someone came");
 
             //if (!Request.Cookies.ContainsKey("KeyPhrase"))
             //    Response.Cookies.Append("KeyPhrase", _configuration["Phrases:Key phrase"]);
